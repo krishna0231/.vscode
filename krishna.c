25f9e16c1fe1208e28a8782add118232699dc127
Krishna saud <krishnasaud864@gmail.com>
2025-01-08 17:39:52 +0000

    its  me coder

--- krishna.c
+++ krishna.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    printf("hello world");
+    int r;
+    float area,circum;
+
+    printf("enter radius of circle :");
+    scanf("%d",&r);
+    area=3.14*r*r;
+    circum=2*3.14*r;
+    printf("area of circum is : %f",area);
+    printf("\nThe circumference of circle is %f",circum);
     return 0;
 }
